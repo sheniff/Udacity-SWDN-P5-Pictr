3d3c5fb05412229b6ff89071b7ab27a0cf925d26
Sergio Almecija <sheniff@gmail.com>
2016-10-06 08:12:53 -0700

    feedback for ghpages

--- ghpublish.js
+++ ghpublish.js
@@ -1,9 +1,11 @@
 // Publish www folder to GH pages
 var ghpages = require('gh-pages');
 var path = require('path');
+var dir = path.join(__dirname, 'platforms', 'browser', 'www');
 
+console.log('Deploying [' + dir + '] to gh-pages...');
 ghpages.publish(
-  path.join(__dirname, 'www'),
+  dir,
   function(err) {
     if (err)
       console.log('Uh oh...', err);
